e3092ba2354c7ba9afc223d6277a0fbc2fa6d225
Wael R. Mansour <wael.r.roushdy@gmail.com>
2023-03-19 20:33:22 -0600

    3 sum

---
+++ NeetCodeRoadMap/TwoPointers/11_ContainerWithMostWater/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+
+
+int MaxArea(int[] height)
+{
+    int maxArea = 0;
+    var l = 0;
+    var r = height.Length;
+
+    return maxArea;
+}
--- NeetCodeRoadMap/TwoPointers/15_3Sum/Program.cs
+++ NeetCodeRoadMap/TwoPointers/15_3Sum/Program.cs
@@ -1,10 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine(ThreeSum(new int[] { -1, 0, 1, 2, -1, -4})); // -4, -1, -1, 0, 1, 2
 
 
 
 IList<IList<int>> ThreeSum(int[] nums)
 {
+    var result = new List<IList<int>>();
+    
+    nums = nums.OrderBy(x => x).ToArray();
 
-    return null;
+    for (int i = 0; i < nums.Length; i++)
+    {
+        var sum = nums[i];
+
+        if (sum > 0)
+            break;
+
+        if (i > 0 && nums[i - 1] == sum)
+            continue;
+
+        int l = i + 1;
+        int r = nums.Length - 1;
+
+        while (r > l)
+        {
+            var total = sum + nums[l] + nums[r];
+
+            if (total > 0)
+                r--;
+            else if (total < 0) 
+                l++;
+            else
+            {
+                var lst = new List<int>() { nums[i], nums[r], nums[l] };
+                result.Add(lst);
+                l++;
+                r--;
+                while (nums[l] == nums[l-1] && l < r)
+                {
+                    l++;
+                }
+            }
+        }
+    }
+
+    return result;
 }
